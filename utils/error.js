dd5ee69a6b617d740ec917686c2e86c3d0201f47
Beverly Hernandez <beverlyandez@gmail.com>
2020-05-10 13:05:01 -0400

    clarified error message

--- utils/error.js
+++ utils/error.js
@@ -2,7 +2,7 @@ module.exports = (msg) => {
   msg.channel.send({
     embed: {
     color: 16697377,
-    description: 'Oops! Did you forget something? Try **/turnip help** for more information!',
+    description: 'Oops! You might have forgotten something, or a flag was invalid. Try **/turnip help** for more information!',
     timestamp: new Date
     }
   }).catch(err => {
